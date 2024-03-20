f36314e2f0195d469f2483aff26f36929c2b8282
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 16:03:33 +0200

    Add project files.

---
+++ TestRUN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestRUN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
